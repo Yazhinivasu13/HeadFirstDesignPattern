3c61884b94968c08857d0a6c7322c52787b4e4a8
Yazhini <yazhini.v@thoughtworks.com>
2022-04-20 21:38:46 +0530

    Ice cream flavour ordering system

---
+++ src/main/java/com/thoughtworks/learning/decorator_pattern/IceCream.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.learning.decorator_pattern;
+
+public abstract class IceCream {
+    int scoops;
+    public abstract String description();
+    public abstract float cost();
+}
---
+++ src/test/java/com/thoughtworks/learning/decorator_pattern/IceCreamTest.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.learning.decorator_pattern;
+
+import org.junit.jupiter.api.Test;
+
+class IceCreamTest {
+
+    @Test
+    void shouldReturnTheCostBasedOnTheFlavourAndTheNoOfScoopsChosenByTheCustomer() {
+        IceCream iceCream = new Chocolate(2);
+        System.out.println(iceCream.description());
+        System.out.println("Cost : Rs."+iceCream.cost());
+    }
+}
